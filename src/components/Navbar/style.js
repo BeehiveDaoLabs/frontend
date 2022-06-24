0df1b4241dc236d314b1dd7347d180c5b4da95ac
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-06-24 20:05:58 +0100

    adjusted  navbar background and color

--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -4,4 +4,21 @@ export const NavbarWrapper = styled.div`
 	width: 100vw;
 	height: 70px;
 	background: red;
+	grid-template-columns: repeat(12, 1fr);
+	background: var(--off-white);
+	/* box-shadow: 5px 5px 5px var(--dark); */
+
+	.logo {
+		grid-column: 1/2;
+		grid-template-columns: repeat(2, 1fr);
+		background: orange;
+		grid-gap: 0 0.5em;
+		align-items: center;
+		color: var(--dark);
+
+		img {
+			width: 40px;
+			height: 40px;
+		}
+	}
 `;
