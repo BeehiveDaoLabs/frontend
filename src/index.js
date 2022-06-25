b779b0cbaf41587fa4ce8c6f0db22c660a314e03
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-06-25 17:06:36 +0100

    wrapped App component in Web3Provider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import { Web3Provider } from "./Context/web3";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<Router>
-			<App />
+			<Web3Provider>
+				<App />
+			</Web3Provider>
 		</Router>
 	</React.StrictMode>
 );
