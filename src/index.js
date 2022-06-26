7fc4fef40dce46f0c189963c66a3ddccafcb36b7
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-06-26 15:19:37 +0100

    wrapped App in 'AppProvider' context prorvider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import { AppProvider } from "./Context/app";
 import { Web3Provider } from "./Context/web3";
 import reportWebVitals from "./reportWebVitals";
 
@@ -9,9 +10,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<Router>
-			<Web3Provider>
-				<App />
-			</Web3Provider>
+			<AppProvider>
+				<Web3Provider>
+					<App />
+				</Web3Provider>
+			</AppProvider>
 		</Router>
 	</React.StrictMode>
 );
