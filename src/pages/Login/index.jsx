188a599bd1e70c066619b8f037ef1b13dd997fd2
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-06-27 09:56:33 +0100

    added login components

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+// import { BiUserCircle } from "react-icons/bi";
+import { LoginWrapper } from "./style";
+import Form from "../../components/Form";
+// import logo from "../../assets/logo/opensea-pride.svg";
 
 function Login() {
-	return <div>Login</div>;
+	return (
+		<LoginWrapper className="grid">
+			<div className="grid container">
+				{/* <header className="grid">
+					<div className="grid logo">
+						<img src={logo} alt="logo" />
+						<h2>Opensea.io</h2>
+					</div>
+				</header> */}
+				<Form
+					title="Create Your Opensea.io Account"
+					// subtitle="Get Started For Free by Signing Up Now."
+					isSignUp={true}
+				/>
+			</div>
+		</LoginWrapper>
+	);
 }
 
 export default Login;
