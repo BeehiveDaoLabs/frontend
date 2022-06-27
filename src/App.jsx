08af151691df01393a7e8df114c5dbce3b755bc1
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-06-27 21:43:23 +0100

    added '/signup' route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<Routes>
 				<Route exact path="/" element={<Home />} />
 				<Route exact path="/login" element={<Login />} />
+				<Route exact path="/signup" element={<Signup />} />
 			</Routes>
 		</div>
 	);
